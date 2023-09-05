1feb01e61a198e1c9aa70c803f1935e3a4bceb94
rangga <ranggantwleng77@gmail.com>
2023-09-05 13:14:55 +0700

    fragment kotlin

---
+++ app/src/main/java/com/rangga/myflexiblefragment/ProfileActivity.kt
@@ -0,0 +1,24 @@
+package com.rangga.myflexiblefragment
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class ProfileActivity : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_profile_activity, container, false)
+    }
+
+
+}
